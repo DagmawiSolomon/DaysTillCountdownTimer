de71694aa7aef444ad95a766744b68338a63292b
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-06-02 09:52:37 +0300

    create a new project

---
+++ daystill/target/debug/deps/daystill-9814a75a399821b5.d
@@ -0,0 +1,5 @@
+/home/dagmawisolomon/Desktop/A-project-A-week-Rust-Edition-/daystill/target/debug/deps/libdaystill-9814a75a399821b5.rmeta: src/main.rs
+
+/home/dagmawisolomon/Desktop/A-project-A-week-Rust-Edition-/daystill/target/debug/deps/daystill-9814a75a399821b5.d: src/main.rs
+
+src/main.rs:
